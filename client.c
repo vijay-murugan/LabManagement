49bbc97cd1eba11477f6fff7d003374560e2aca6
vijay-murugan <56683057+vijay-murugan@users.noreply.github.com>
2021-05-04 20:37:42 +0530

    Add files via upload

---
+++ client.c
@@ -0,0 +1,12 @@
+#include "header.h"
+
+int main()
+{
+    system("gcc .imp.c");
+    system("./a.out");
+    printf("                                    Welcome to Chemistry Lab Mangment system\n              ");
+    printf("\n\n");
+    login_page();
+    
+    return 0;
+}
